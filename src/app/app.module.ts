3bd1e80e0121e95e158d5eadaa773e5ce9d0b742
themba mncube <themba@webrave.co.za>
2020-10-10 14:35:53 +0200

    paypage final update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { PayPageComponent } from './pay-page/pay-page.component';
 import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
 import { ContactComponent } from './contact/contact.component';
+import { ApproveComponent } from './approve/approve.component';
 
 
 @NgModule({
@@ -24,6 +25,7 @@ import { ContactComponent } from './contact/contact.component';
     AboutComponent,
     ServicesComponent,
     ContactComponent,
+    ApproveComponent,
     
   ],
   imports: [
